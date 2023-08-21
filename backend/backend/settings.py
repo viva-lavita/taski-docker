ef87b23be497ac9982a9e10430e52787db626851
Виктория Сопина <vt.sopina@yandex.ru>
2023-08-21 17:33:31 +0300

    Add new workfollow

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -60,10 +61,23 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+
+   'default': {
+
+       'ENGINE': 'django.db.backends.postgresql',
+
+       'NAME': os.getenv('POSTGRES_DB', 'django'),
+
+       'USER': os.getenv('POSTGRES_USER', 'django'),
+
+       'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+
+       'HOST': os.getenv('DB_HOST', ''),
+
+       'PORT': os.getenv('DB_PORT', 5432)
+
+   }
+
 }
 
 
@@ -104,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
